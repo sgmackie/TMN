5ea6c4c667af4ae7cd8d4edafd3a64e14c779520
sgmackie <sam.g.mackie@googlemail.com>
2021-09-01 01:11:18 +0100

    Add RemedyBG VSCode tasks

--- code/tools/unit_testing/unit_tester.c
+++ code/tools/unit_testing/unit_tester.c
@@ -7,7 +7,6 @@ int main(int argc, char **argv) {
 
     // Load modules
     platform->dll->Open(argv[1]);
-
     // MemoryArena arena = CreateMemoryArena();
     // f32 *array = arena.allocator.Reallocate(&arena.allocator, 0, sizeof(f32) * 64);
     return 0;
